0fbd50bf3c71073a5031123793181bb95b5601d9
Riley <riley.s2003@hotmail.com>
2026-06-11 17:12:34 -0500

    more work on task management functionality in app.js

--- src/App.js
+++ src/App.js
@@ -1,37 +1,37 @@
 import TaskContainer from './components/TaskContainer';
 import AddTask from './components/AddTask';
+import { useReducer, useEffect } from "react";
 
-import { useReducer } from "react";
-
-const updateTask = (task, action) => {
-    switch(action.type) {
+const taskReducer = (state, action) => {
+    switch (action.type) {
         case "ADD":
-            break;
-        case "EDIT":
-            break;
+            return [...state, action.payload];
         case "DELETE":
-            break;
+            return state.filter(task => task.id !== action.payload);
         case "COMPLETE":
-            break;
-        default: 
-            return;
+            return state.map(task => task.id === action.payload ? { ...task, isComplete: !task.isComplete } : task);
+        case "EDIT":
+            return state.map(task => task.id === action.payload.id ? { ...task, title: action.payload.newTitle, date: "2026-06-11" } : task);
+        default:
+            return state;
     }
 }
 
 function App() {
-    const [task, dispatch] = useReducer(addTask, 0);
+    const [tasks, dispatch] = useReducer(taskReducer, [], () => {
+        const savedTasks = localStorage.getItem("tasks");
+        
+        return savedTasks ? JSON.parse(saved) : [];
+    });
  
-    let tasks = [{
-        id: 0,
-        title: "",
-        date: "2025-06-01",
-        isComplete: false
-    }];
+    useEffect(() => {
+        localStorage.setItem("tasks", JSON.stringify(tasks));
+    }, [tasks]);
 
     return (
         <div className="container">
-            <AddTask updateTask={updateTask}/>
-            <TaskContainer tasks={tasks}/>
+            <AddTask dispatch={dispatch}/>
+            <TaskContainer tasks={tasks} disptatch={dispatch}/>
         </div>
     );
 }
